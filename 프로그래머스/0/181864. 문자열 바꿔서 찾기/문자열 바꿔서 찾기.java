a293bbdb89b2a3b4a42bbea01d2b77eb1b9f79dd
Akaps0613 <minseokjo7777@gmail.com>
2025-08-06 14:02:11 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 3.10 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        String str = "";
+        for(int i=0;i<myString.length();i++){
+            if(myString.charAt(i)=='A'){
+                str = str+"B";
+            }
+            else 
+                str = str+"A";
+        }
+        if(str.contains(pat)) return 1;
+        return 0;
+    }
+}
