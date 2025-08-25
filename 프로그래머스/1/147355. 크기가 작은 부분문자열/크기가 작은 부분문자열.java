ed94b05de6b322cecb08d61853cab772a118723b
Akaps0613 <minseokjo7777@gmail.com>
2025-08-25 10:55:23 -0400

    [level 1] Title: 크기가 작은 부분문자열, Time: 11.66 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        for(int i=0;i<t.length()-p.length()+1;i++){
+            String x = t.substring(i,i+p.length());
+            if(Long.valueOf(x)<=Long.valueOf(p)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
